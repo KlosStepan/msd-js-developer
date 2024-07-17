46d18bfa38165ac075288744d6963978239f0efa
Stepan Klos <stepanklos@gmail.com>
2024-07-17 15:59:24 +0200

    Basic Layout

--- README.md
+++ README.md
@@ -34,3 +34,7 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+## Misc
+- import problem https://medium.com/@fdikmen/fixing-could-not-find-the-module-error-in-nextjs-with-ant-design-3ae2cfe0160d
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,78 @@
+import React from 'react';
 import Image from "next/image";
 import styles from "./page.module.css";
+import 'antd/dist/reset.css';
+import { Button, Flex, Layout } from "antd";
+import { Header, Footer, Content } from "antd/lib/layout/layout"; //, Sider
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'left',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  flex: 1,
+  overflow: 'auto',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+
+const layoutStyle: React.CSSProperties = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh', // Ensures the layout takes the full height of the viewport
+};
+
+// Additional styles
+const pageTitleStyle: React.CSSProperties = {
+  textAlign: 'left',
+  paddingInline: 48,
+  width: '100%',
+  backgroundColor: '#0a74da',
+  padding: '10px',
+  boxSizing: 'border-box',
+};
+
+const boxContainerStyle: React.CSSProperties = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '10px',
+};
+
+const boxStyle: React.CSSProperties = {
+  width: '48%', // Slightly less than 50% to account for spacing
+  backgroundColor: '#4caf50',
+  padding: '10px',
+  boxSizing: 'border-box',
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <React.Fragment>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>App title</Header>
+        <Content style={contentStyle}>
+          <div style={pageTitleStyle}>Page title - |Export to PDF| |Notes (3)| |Filter v|</div>
+          <div style={boxContainerStyle}>
+            <div style={boxStyle}>1. part 50% width box</div>
+            <div style={boxStyle}>2. part 50% width box</div>
+          </div>
+        </Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </React.Fragment>
   );
 }
